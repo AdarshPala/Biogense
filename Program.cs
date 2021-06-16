429047d89ba3688fa3ccd1b08099c2f778d3f6ea
adarsh pala <adarsh_pala@hotmail.com>
2021-06-16 19:07:40 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Biogense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
